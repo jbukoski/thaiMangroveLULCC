2c854e44c866a4c3c16ca512e9d5d410433f9aa5
jbukoski <jacob.bukoski@yale.edu>
2020-09-07 19:33:15 -0600

    updating files

--- scripts/modeling.R
+++ scripts/modeling.R
@@ -14,8 +14,6 @@ scratch_dir <- "./data/scratch/"
 
 source("./scripts/models.R")
 
-
-
 ###################################################################
 ## Calculate carbon stock losses and gains at the national scale ##
 ###################################################################
@@ -23,8 +21,8 @@ source("./scripts/models.R")
 # Load in data
 
 dstrcts_c_df <- st_read(paste0(proc_dir, "shapefiles/dstrcts_c/")) %>%
-  st_set_geometry(NULL)
-
+  st_set_geometry(NULL) %>%
+  arrange(ADM2_EN)
 
 mg2000 <- st_read(paste0(proc_dir, "shapefiles/dstrct_ttls_2000")) %>%
   left_join(dstrcts_c_df, by = c("ADM2_EN", "ADM1_EN", "ADM2_ID"))
@@ -45,120 +43,89 @@ mg2014_gn <- st_read(paste0(proc_dir, "shapefiles/dstrct_gains_2014")) %>%
   mutate(ttl_gn = sum(aqucltr, agrcltr, othr_fr, mudflts, abandnd, slt_frm, urban, water, nodata, na.rm = T)) %>%
   left_join(dstrcts_c_df, by = c("ADM2_EN", "ADM1_EN", "ADM2_ID"))
 
-# Add the emission factors based on the randomly assigned activity years and models
 
-dstrcts_c_df <- dstrcts_c_df %>%
-  rowwise() %>%
-  mutate(activity_yr = 2014 - round(runif(1, min = 2000, max = 2013), 0),
-         AGB_LS_EF = AGB_AVG * exp(coef(agb_ls_mdl)[1] + coef(agb_ls_mdl)[2] * activity_yr),
-         AGB_LS_EF_SE = AGB_SE * exp(coef(agb_ls_mdl)[1] + coef(agb_ls_mdl)[2] * activity_yr),
-         SOC_LS_EF = SOC_AVG * exp(coef(soc_ls_mdl)[1]  + coef(soc_ls_mdl)[2] * activity_yr),
-         SOC_LS_EF_SE = SOC_SE * exp(coef(soc_ls_mdl)[1]  + coef(soc_ls_mdl)[2] * activity_yr),
-         AGB_GN_EF = coef(agb_gn_mdl)[1]  / (1 + coef(agb_gn_mdl)[2] * (exp(1) ^ (-coef(agb_gn_mdl)[3] * activity_yr))),
-         AGB_GN_EF_SE = summary(agb_gn_mdl)[["coefficients"]][1, 2]  / (1 + summary(agb_gn_mdl)[["coefficients"]][2, 2] * exp(1) ^ summary(agb_gn_mdl)[["coefficients"]][3, 2] * activity_yr),
-         SOC_GN_EF = SOC_AVG * exp(coef(soc_gn_mdl)[1]  + coef(soc_gn_mdl)[2] * log(activity_yr)),
-         SOC_GN_EF_SE = SOC_SE * exp(coef(soc_gn_mdl)[1]  + coef(soc_gn_mdl)[2] * log(activity_yr)),
-         SEQ_EF = 1.74 * activity_yr,
-         SEQ_EF_SE = 0.23 * activity_yr,
-         ttl_ls_ef = AGB_LS_EF + SOC_LS_EF,
-         ttl_ls_ef_se = AGB_LS_EF_SE + SOC_LS_EF_SE,
-         ttl_gn_ef = AGB_GN_EF + SOC_GN_EF,
-         ttl_gn_ef_se = AGB_GN_EF_SE + SOC_GN_EF_SE)
+#-----------------------------
+# Begin simulating carbon gains and losses
 
 
-mg2014_ls_c <- mg2014_ls %>%
-  mutate(aqucltr_c = aqucltr * ttl_ls_ef,
-         aqucltr_c_se = aqucltr * ttl_ls_ef_se,
-         agrcltr_c = agrcltr * ttl_ls_ef,
-         agrcltr_c_se = agrcltr * ttl_ls_ef_se,
-         othr_fr_c = othr_fr * ttl_ls_ef,
-         othr_fr_c_se = othr_fr * ttl_ls_ef_se,
-         mudflts_c = mudflts * ttl_ls_ef,
-         mudflts_c_se = mudflts * ttl_ls_ef_se,
-         abandnd_c = abandnd * ttl_ls_ef,
-         abandnd_c_se = abandnd * ttl_ls_ef_se,
-         slt_frm_c = slt_frm * ttl_ls_ef,
-         slt_frm_c_se = slt_frm * ttl_ls_ef_se,
-         urban_c = urban * ttl_ls_ef,
-         urban_c_se = urban * ttl_ls_ef_se,
-         water_c = water * ttl_ls_ef,
-         water_c_se = water * ttl_ls_ef_se,
-         nodata_c = nodata * ttl_ls_ef,
-         nodata_c_se = nodata * ttl_ls_ef_se,
-         seq_c = ttl_ls * SEQ_EF,
-         seq_c_se = ttl_ls * SEQ_EF_SE,
-         undist_c = ttl_ls * AGB_AVG + ttl_ls * SOC_AVG + seq_c) %>%
-  dplyr::select(ADM1_EN, ADM2_EN, activity_yr, aqucltr_c:undist_c) %>%
-  mutate(ttl_ls_c = sum(aqucltr_c, agrcltr_c, othr_fr_c, mudflts_c, abandnd_c, slt_frm_c, urban_c, water_c, nodata_c, na.rm = T),
-         ttl_ls_c_se = sum(aqucltr_c_se, agrcltr_c_se, othr_fr_c_se, mudflts_c_se, abandnd_c_se, slt_frm_c_se, urban_c_se, water_c_se, nodata_c_se, na.rm = T),
-         net_ls_c = undist_c - ttl_ls_c)
+summary <- data.frame()
 
-mg2014_ls_c %>%
-  dplyr::select(undist_c, ttl_ls_c, net_ls_c) %>%
-  ungroup() %>%
-  summarize(undist_c = sum(undist_c, na.rm = T),
-            ttl_ls_c = sum(ttl_ls_c, na.rm = T),
-            net_ls_c = sum(net_ls_c, na.rm = T))
+for(i in 1:nrow(dstrcts_c_df)) {
+  
+  vals <- c()
+  dstrct <- dstrcts_c_df[i,]
+  dstrct_ls <- mg2014_ls[i,]
+  dstrct_gn <- mg2014_gn[i,]
+  
+  for(j in 1:3000) {
+    
+    act_yr <- round(runif(1, min = 1, max = 14), 0)
+    agb_avg <- rnorm(1, mean = dstrct$AGB_AVG, sd = dstrct$AGB_SE * sqrt(40))
+    agb_avg <- ifelse(agb_avg > 0, agb_avg, 0)
+    soc_avg <- rnorm(1, mean = dstrct$SOC_AVG, sd = dstrct$SOC_SE * sqrt(40))
+    soc_avg <- ifelse(soc_avg > 0, soc_avg, 0)
+    
+    agb_ls_coefs <- c(rnorm(1, mean = as.data.frame(summary(agb_ls_mdl)[[4]])[1, 1], sd = as.data.frame(summary(agb_ls_mdl)[[4]])[1, 2] * sqrt(4)),
+                      rnorm(1, mean = as.data.frame(summary(agb_ls_mdl)[[4]])[2, 1], sd = as.data.frame(summary(agb_ls_mdl)[[4]])[2, 2] * sqrt(4)))
+    
+    for(k in 1:length(agb_ls_coefs)) {
+      while(agb_ls_coefs[k] > 0) { agb_ls_coefs[k] <- rnorm(1, mean = as.data.frame(summary(agb_ls_mdl)[[4]])[k, 1], sd = as.data.frame(summary(agb_ls_mdl)[[4]])[k, 2] * sqrt(4)) }
+    }
+    
+    soc_ls_coefs <- c(rnorm(1, mean = as.data.frame(summary(soc_ls_mdl)[[4]])[1, 1], sd = as.data.frame(summary(soc_ls_mdl)[[4]])[1, 2] * sqrt(34)),
+                      rnorm(1, mean = as.data.frame(summary(soc_ls_mdl)[[4]])[2, 1], sd = as.data.frame(summary(soc_ls_mdl)[[4]])[2, 2] * sqrt(34)))
+    
+    for(k in 1:length(soc_ls_coefs)) {
+      while(soc_ls_coefs[k] > 0) { soc_ls_coefs[k] <- rnorm(1, mean = as.data.frame(summary(soc_ls_mdl)[[4]])[k, 1], sd = as.data.frame(summary(soc_ls_mdl)[[4]])[k, 2] * sqrt(34)) }
+    }
+    
+    agb_gn_coefs <- c(rnorm(1, mean = as.data.frame(summary(agb_gn_mdl)[[10]])[1, 1], sd = as.data.frame(summary(agb_gn_mdl)[[10]])[1, 2] * sqrt(59)),
+                      rnorm(1, mean = as.data.frame(summary(agb_gn_mdl)[[10]])[2, 1], sd = as.data.frame(summary(agb_gn_mdl)[[10]])[2, 2] * sqrt(59)),
+                      rnorm(1, mean = as.data.frame(summary(agb_gn_mdl)[[10]])[3, 1], sd = as.data.frame(summary(agb_gn_mdl)[[10]])[3, 2] * sqrt(59)))
+    
+    for(k in 1:length(agb_gn_coefs)) {
+      while(agb_gn_coefs[k] < 0) { agb_gn_coefs[k] <- rnorm(1, mean = as.data.frame(summary(agb_gn_mdl)[[10]])[k, 1], sd = as.data.frame(summary(agb_gn_mdl)[[10]])[k, 2] * sqrt(59)) }
+    }
+    
+    soc_gn_coefs <- c(rnorm(1, mean = as.data.frame(summary(soc_gn_mdl)[[12]])[1, 1], sd = as.data.frame(summary(soc_gn_mdl)[[12]])[1, 2] * sqrt(22)),
+                      rnorm(1, mean = as.data.frame(summary(soc_gn_mdl)[[12]])[2, 1], sd = as.data.frame(summary(soc_gn_mdl)[[12]])[2, 2] * sqrt(22)))
+    
+    while(soc_gn_coefs[1] > 0) { soc_gn_coefs[1] <- rnorm(1, mean = as.data.frame(summary(soc_gn_mdl)[[12]])[1, 1], sd = as.data.frame(summary(soc_gn_mdl)[[12]])[1, 2] * sqrt(22)) }
+    while(soc_gn_coefs[2] < 0) { soc_gn_coefs[2] <- rnorm(1, mean = as.data.frame(summary(soc_gn_mdl)[[12]])[2, 1], sd = as.data.frame(summary(soc_gn_mdl)[[12]])[2, 2] * sqrt(22)) }
+    
+    agc_prsrvd <- agb_avg * exp(agb_ls_coefs[1] + agb_ls_coefs[2] * act_yr)
+    soc_prsrvd <- soc_avg * exp(soc_ls_coefs[1] + soc_ls_coefs[2] * act_yr)
+    agc_gained <- agb_gn_coefs[1]  / (1 + agb_gn_coefs[2] * (exp(1) ^ (-agb_gn_coefs[3] * act_yr)))
+    soc_gained <- soc_avg * exp(soc_gn_coefs[1]  + soc_gn_coefs[2] * log(act_yr))
+    frgn_sqstr <- 1.5 * act_yr
+    
+    mgc_loss <- -1 * (agb_avg + soc_avg + frgn_sqstr - agc_prsrvd - soc_prsrvd)
+    mgc_rcvr <- agc_gained + soc_gained - agc_prsrvd - soc_prsrvd
+    mgc_expn <- agc_gained + soc_gained
+    
+    vals <- rbind(vals, c(dstrct$ADM2_ID, j, dstrct_ls$ttl_ls, dstrct_gn$ttl_gn - dstrct_gn$nodata, dstrct_gn$nodata, mgc_loss, mgc_rcvr, mgc_expn))
+    
+  }
+  
+  vals_df <- as.data.frame(vals) 
+  
+  colnames(vals_df) <- c("ADM2_ID", "j", "LOSS", "GAIN", "NODATA", "MGC_LOSS", "MGC_RCVR", "MGC_GAIN")
+  
+  dstrct_df <- vals_df %>%
+    left_join(dplyr::select(dstrcts_c_df, ADM1_EN:ADM2_ID)) %>%
+    mutate(MGC_LOSS_AVG = mean(MGC_LOSS, na.rm = T),
+           MGC_LOSS_SE = plotrix::std.error(MGC_LOSS, na.rm = T),
+           MGC_RCVR_AVG = mean(MGC_RCVR, na.rm = T),
+           MGC_RCVR_SE = plotrix::std.error(MGC_RCVR, na.rm = T),
+           MGC_GAIN_AVG = mean(MGC_GAIN, na.rm = T),
+           MGC_GAIN_SE = plotrix::std.error(MGC_GAIN, na.rm = T)) %>%
+    dplyr::select(ADM1_EN, ADM2_EN, ADM2_ID, LOSS:NODATA, MGC_LOSS_AVG:MGC_GAIN_SE) %>%
+    mutate(GAIN = ifelse(is.na(GAIN), 0, GAIN),
+           NODATA = ifelse(is.na(NODATA), 0, NODATA)) %>%
+    distinct() %>%
+    mutate(NET_MGC = (LOSS * MGC_LOSS_AVG) + (GAIN * MGC_RCVR_AVG) + (NODATA * MGC_GAIN_AVG),
+           NET_MGC_SE = (LOSS * MGC_LOSS_SE) + (GAIN * MGC_RCVR_SE) + (NODATA * MGC_GAIN_SE))
+  
+  summary <- rbind(summary, dstrct_df)
 
-
-mg2014_gn_c <- mg2014_gn %>%
-  mutate(aqucltr_c = aqucltr * ttl_gn_ef,
-         aqucltr_c_se = aqucltr * ttl_gn_ef_se,
-         agrcltr_c = agrcltr * ttl_gn_ef,
-         agrcltr_c_se = agrcltr * ttl_gn_ef_se,
-         othr_fr_c = othr_fr * ttl_gn_ef,
-         othr_fr_c_se = othr_fr * ttl_gn_ef_se,
-         mudflts_c = mudflts * ttl_gn_ef,
-         mudflts_c_se = mudflts * ttl_gn_ef_se,
-         abandnd_c = abandnd * ttl_gn_ef,
-         abandnd_c_se = abandnd * ttl_gn_ef_se,
-         slt_frm_c = slt_frm * ttl_gn_ef,
-         slt_frm_c_se = slt_frm * ttl_gn_ef_se,
-         urban_c = urban * ttl_gn_ef,
-         urban_c_se = urban * ttl_gn_ef_se,
-         water_c = water * ttl_gn_ef,
-         water_c_se = water * ttl_gn_ef_se,
-         nodata_c = nodata * ttl_gn_ef,
-         nodata_c_se = nodata * ttl_gn_ef_se,
-         seq_c = ttl_gn * SEQ_EF,
-         seq_c_se = ttl_gn * SEQ_EF_SE) %>%
-  dplyr::select(ADM1_EN, ADM2_EN, activity_yr, aqucltr_c:seq_c_se) %>%
-  mutate(ttl_gn_c = sum(aqucltr_c, agrcltr_c, othr_fr_c, mudflts_c, abandnd_c, slt_frm_c, urban_c, water_c, nodata_c, na.rm = T) - seq_c,
-         ttl_gn_c_se = sum(aqucltr_c_se, agrcltr_c_se, othr_fr_c_se, mudflts_c_se, abandnd_c_se, slt_frm_c_se, urban_c_se, water_c_se, nodata_c_se, na.rm = T) - seq_c_se)
-
-mg2014_ls %>%
-  mutate(woulda_been = (ttl_ls * AGB_AVG) + (ttl_ls * SOC_AVG)) %>%
-  pull(woulda_been) %>%
-  sum(na.rm = T)
-
-mg2014_gn_c %>%
-  pull(ttl_gn_c) %>%
-  sum(na.rm = T)
-
-mg2014_ls_c %>%
-  pull(ttl_ls_c) %>%
-  sum(na.rm = T)
-
-mg2014_gn %>%
-  mutate(extant_c_2014 = mangrov * AGB_AVG + mangrov * SOC_AVG) %>%
-  pull(extant_c_2014) %>%
-  sum(na.rm = T)
-
-
-mg2000 %>%
-  st_set_geometry(NULL) %>%
-  dplyr::select(ADM1_EN, ADM2_EN, mangrove_2000 = mangrov) %>%
-  left_join(dplyr::select(st_set_geometry(mg2014, NULL), ADM1_EN, ADM2_EN, mangrove_2014 = mangrov)) %>%
-  left_join(dstrcts_c_df) %>%
-  mutate(mg_diff = mangrove_2000 - mangrove_2014,
-         agb_ls = mg_diff * AGB_LS_EF,
-         agb_ls_se = mg_diff * AGB_LS_EF_SE,
-         soc_ls = mg_diff * SOC_LS_EF,
-         soc_ls_se = mg_diff * SOC_LS_EF_SE,
-         ttl_ls = agb_ls + soc_ls,
-         ttl_ls_se = agb_ls_se + soc_ls_se,
-         woulda_been_2000 = mg_diff * AGB_AVG + mg_diff * SOC_AVG,
-         c_2000 = mangrove_2000 * AGB_AVG + mangrove_2000 * SOC_AVG,
-         c_2000_se = mangrove_2000 * AGB_SE + mangrove_2000 * SOC_SE) %>%
-  pull(woulda_been_2000) %>%
-  sum(na.rm = T)
+  }
